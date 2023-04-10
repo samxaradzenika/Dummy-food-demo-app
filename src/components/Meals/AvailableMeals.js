a3d4feeed79db7419f919d767fe8c59442b5e6a3
samxaradzenika <murdoc746@gmail.com>
2023-04-11 03:07:23 +0400

    Meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,7 @@
+import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 const DUMMY_MEALS = [
    {
       id: 'm1',
@@ -26,10 +30,19 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-   const mealsList = DUMMY_MEALS.map((meals) => <li>{meals.name}</li>);
+   const mealsList = DUMMY_MEALS.map((meals) => (
+      <MealItem
+         key={meals.id}
+         name={meals.name}
+         description={meals.description}
+         price={meals.price}
+      />
+   ));
    return (
-      <section>
-         <ul>{mealsList}</ul>
+      <section className={classes.meals}>
+         <Card>
+            <ul>{mealsList}</ul>
+         </Card>
       </section>
    );
 };
